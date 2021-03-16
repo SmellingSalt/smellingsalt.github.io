3f813c831a70a054948156747323f14755ef5f56
Sawan Singh Mahara <ssmahara96@gmail.com>
2021-03-16 13:21:16 +0530

    updates

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Memorylessness of the Geometric Random Variable",
+        "excerpt":"Consider the case of winning a lottery. Either you win it or you don’t. Let the probability of winning the lottery be \\(p\\) and the probability of losing it be \\((1-p)\\) My strategy is that I keep buying tickets until I win. Then I stop. If I ask the question...","categories": ["Blog"],
+        "tags": ["Probability","Problems"],
+        "url": "http://localhost:4000/blog/Geometric-Random-Variable/",
+        "teaser": null
+      }]
